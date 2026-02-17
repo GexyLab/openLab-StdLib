cbf75cc699d980b5c149ba48fd191d5719cf10b8
Eugenio Liguori <e.liguori@gexy.it>
2026-02-17 22:59:40 +0100

    Add some math operations

---
+++ math/LBNegate.cs
@@ -0,0 +1,79 @@
+﻿using OpenLabSDK.config;
+using OpenLabSDK.error;
+using OpenLabSDK.events;
+using OpenLabSDK.instruments;
+using OpenLabSDK.plugin;
+using OpenLabSDK.ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace OpenLabStdLib.math
+{
+    public class LBNegate : LogicBlock
+    {
+        public class Info : LogicBlockInfo
+        {
+            public string UID => "";
+
+            public string Title => "Negate of value (-x)";
+
+            public int[] version => [1, 0, 0];
+
+            public string vendorUID => "";
+
+            public string url => "";
+
+            public string description => "Negate of value of port IN";
+
+            public string instrumentsMenuCategoryTitle => "Math";
+        }
+
+        LogicBlockPort inputPortA,outputPort;
+
+        public LBNegate(IErrorManager _errorManager, IPluginsManager _pluginsManager, IEventsManager _eventsManager, IWindowsManager _windowsManager, InstrumentDefinition instrumentDefinition) : base(_errorManager, _pluginsManager, _eventsManager, _windowsManager, instrumentDefinition)
+        {   
+            logicBlockInfo = new Info();
+
+            Type type = typeof(float);
+
+            inputPortA = new LogicBlockPort("IN", type, LogicBlockPort.DataDirection.Input);
+            inputPortA.settings.horizontalPosition = OpenLabSDK.logicblocks.LogicBlockPortDefaultSettings.Position.Left;
+            inputPortA.settings.onDataChangeOperation = LogicBlockPort.OnDataChangeOperations.ExecuteLogicBlockRunMethod;
+            addPort(inputPortA);
+
+            inputPortA.settings.dataValueViewerVisibilityChangeByUser = true;
+            inputPortA.settings.dataValueEditable = true;
+
+            outputPort = new LogicBlockPort("OUT", type, LogicBlockPort.DataDirection.Output);
+            outputPort.settings.horizontalPosition = OpenLabSDK.logicblocks.LogicBlockPortDefaultSettings.Position.Right;
+            outputPort.settings.onDataChangeOperation = LogicBlockPort.OnDataChangeOperations.PutDataViaConnection;  
+            addPort(outputPort);
+
+            settings.toStartOperation = LogicBlock.ToStartOperations.ExecuteRunMethod;
+        }
+
+        private void InputPortOnDataChange(object source, LogicBlockPortDataChangeEventArgs e)
+        {
+            run();      
+        }
+
+
+        public override void run()
+        {
+            if (inputPortA.getData(false) != null)
+            {
+                outputPort.setData(-(inputPortA.getData(false)), false, false);
+
+            }
+        }
+
+        public override void stop()
+        {
+        }
+    }
+}
